a05e2853fe7d6ed29ac13d21af263becb84aef3b
Dante Sparrås <github.pleading597@passmail.com>
2026-01-20 20:38:03 +0100

    feat: add main header component

---
+++ src/components/main-header.tsx
@@ -0,0 +1,17 @@
+export function MainHeader({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className?: string;
+}) {
+  // Wrap <header> in a <div> with 'sticky' positioning to make it follow the
+  // overscroll behavior of the page. Otherwise, the header would need to have
+  // 'absolute' (not 'sticky' since that would make the <header> disappear when
+  // scrolling) positioning which would make it ignore overscroll behavior.
+  return (
+    <div className="sticky top-0 z-50 w-full">
+      <header className={className}>{children}</header>
+    </div>
+  );
+}
